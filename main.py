d7703afb8ec3cb41a99124f92f2912f0e7cfbde0
Vaibhav Bhardwaj <bhardwajvbhv@gmail.com>
2020-03-15 20:48:52 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+import requests
+import os
+
+def main():
+    client = boto3.client('sns', 'us-east-1')
+    mobile_no = os.environ["INPUT_MOBILE_NO"]
+    msg = "A push just happened to you repo"
+    client.publish(PhoneNumber=mobile_no, Message=msg)
+
+if __name__ == "__main__":
+    main()
