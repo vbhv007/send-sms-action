288363cb7789602beb34c9d3459a88895b0ef2e9
Vaibhav Bhardwaj <bhardwajvbhv@gmail.com>
2020-03-15 21:21:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import boto3
 import os
 
 def main():
-    client = boto3.client('sns', 'us-east-1')
-    mobile_no = os.environ["INPUT_MOBILE_NO"]
+    mobile_no = os.environ["MOBILE_NO"]
+    aws_key_id = os.environ["AWS_KEY_ID"]
+    aws_key_access = os.environ["AWS_KEY_ACCESS"]
+    client = boto3.client(
+        'sns',
+        'us-east-1',
+        aws_access_key_id=aws_key_id,
+        aws_secret_access_key=aws_key_access
+    )
     msg = "A push just happened to you repo"
     client.publish(PhoneNumber=mobile_no, Message=msg)
 
