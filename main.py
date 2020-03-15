6a38188ac244c6a47f32a93ad6b4c011c153e4a6
Vaibhav Bhardwaj <bhardwajvbhv@gmail.com>
2020-03-15 22:51:08 +0530

    correct logic added

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ def main():
     mobile_no = os.environ["INPUT_MOBILE_NO"]
     aws_key_id = os.environ["INPUT_AWS_KEY_ID"]
     aws_key_access = os.environ["INPUT_AWS_KEY_ACCESS"]
-    client = boto3.client(
-        'sns',
-        'us-east-1',
-        aws_access_key_id=aws_key_id,
-        aws_secret_access_key=aws_key_access
-    )
-    msg = "A push just happened to you repo"
-    client.publish(PhoneNumber=mobile_no, Message=msg)
+    client = boto3.client('sns', 'us-east-1', aws_access_key_id=aws_key_id, aws_secret_access_key=aws_key_access)
+    topic = client.create_topic(Name="notifications")
+    topic_arn = topic['TopicArn']
+    client.subscribe(TopicArn=topic_arn, Protocol='sms', Endpoint=mobile_no)
+    msg = "A new push just happened in your repo."
+    client.publish(TopicArn=topic_arn, Message=msg)
+
 
 if __name__ == "__main__":
     main()
